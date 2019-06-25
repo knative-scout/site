04501e91baaccaced5fc396c824f823df153a0b1
Janie Walter <janiewalter12@gmail.com>
2019-06-25 15:50:50 -0400

    Pagination, Responsive AppGrid Layout

--- src/components/AppGrid.tsx
+++ src/components/AppGrid.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useAppList } from '../hooks/Hooks';
 import { AppTile } from './AppTile';
-import { EmptyState,EmptyStateBody} from '@patternfly/react-core';
+import { EmptyState,EmptyStateBody,Pagination} from '@patternfly/react-core';
 import ServerlessApp from '../interfaces/Interfaces';
 
 interface AGProps {
@@ -14,14 +14,36 @@ interface AGProps {
  */
 export const AppGrid : React.FC<AGProps> = (props) => {
 
+    const [perPage,setPerPage] = useState(20);
+    const [page,setPage] = useState(1);
+
+    const onSetPage = (_event : any, pageNumber : number) => {
+        setPage(pageNumber);
+    }
+
+    const onPerPageSelect = (_event : any , newPerPage : number) => {
+        setPerPage(newPerPage);
+    }
+
     return(
         <div className="ks-appgrid">
-            {props.appList == null || props.appList.length == 0? empty :
-                props.appList.map( app => {
-                    return (
-                            <AppTile key={app.app_id} app={app} tagHook={props.tagHook}></AppTile>
-                    );
-                })}
+            <div className="ks-appgrid__control">
+                <Pagination
+                    itemCount={props.appList.length}
+                    perPage={perPage}
+                    page = {page}
+                    onSetPage={onSetPage}
+                    onPerPageSelect={onPerPageSelect}
+                />
+            </div>
+            <div className="ks-appgrid__grid">
+                {props.appList == null || props.appList.length == 0? empty :
+                    props.appList.map( app => {
+                        return (
+                                <AppTile key={app.app_id} app={app} tagHook={props.tagHook}></AppTile>
+                        );
+                    })}
+            </div>
         </div>
     );
 }
