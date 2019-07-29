9bd888d8153ea8d7849e7011d187d3c6c297cf5b
Noah Huppert <contact@noahh.io>
2019-07-29 15:26:18 -0400

    (wip) new app details page

--- src/components/AppDetails/AppDetailsSidebar.tsx
+++ src/components/AppDetails/AppDetailsSidebar.tsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import ServerlessApp from '../../interfaces/Interfaces';
-import { Stack, StackItem, Button, Modal, ClipboardCopy, ClipboardCopyVariant} from '@patternfly/react-core';
+import { Stack, StackItem, Button, Modal, ClipboardCopy, ClipboardCopyVariant, Card, CardHeader, CardBody, Label } from '@patternfly/react-core';
+import { FaLink } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 import {Map2} from '../../utils/Utils';
 import { useDeployInstructions } from '../../hooks/Hooks';
 import Markdown from 'react-markdown';
@@ -9,66 +11,141 @@ import Markdown from 'react-markdown';
 interface DBProps {
     appID : string
 }
-const DeployButton : React.FunctionComponent<DBProps> = (props : DBProps) => {
 
-    const [isModalOpen,setIsModalOpen] = useState(false);
-    const deployInstructions = useDeployInstructions(props.appID);
-
-    const Code = ((props : any) => <ClipboardCopy variant={ClipboardCopyVariant.expansion} isReadOnly>{props.value}</ClipboardCopy>);
-    
-    return (<div>
-         <Button onClick={() => setIsModalOpen(true)}variant="primary">
-             Deploy
-         </Button>
-         <Modal
-             isSmall
-             title="Deployment Instructions"
-             isOpen={isModalOpen}
-             onClose={() => {
-                 setIsModalOpen(false);
-             }}
-         >
-            <Markdown className="ks-markdown" renderers={{code : Code}}source={deployInstructions}/>
-         </Modal>
-     </div>);
- }
+function getLabels(items : string[]) {
+    return (items.map((item: string) => (
+	   <Label>
+		  {item}
+	   </Label>
+    )))
+}
 
 /**
  * Sidebar for App Details page
  */
 export function AppDetailsSidebar (props : { app : ServerlessApp }) {
+    const app = props.app;
 
-    const {app_id, name, version, author, logo_url, tagline, description, tags, maintainer, categories, verification_status, github_url, screenshots_urls, deployment_file_urls} = props.app;
+    /*
+       {name}
+       {tagline}
+
+       ----
+
+       [[deploy ]]
+
+       ----
+
+       [homepage]({homepage_url})
+       [deployment source]({github_url})
+
+       {author.name} <[mailto]({author.email})>
+
+       {tags}
+       {categories}
+    */
+
+    return(<div>
+	   <Card className="ks-appdetails__sidebar__card">
+		  <CardHeader>
+			 {app.name}
+		  </CardHeader>
+
+		  <CardBody>
+			 <h1>{app.name}</h1>
+			 <span>{app.tagline}</span>
+		  </CardBody>
+	   </Card>
+
+	   <Card className="ks-appdetails__sidebar__card">
+		  <CardBody>
+			 <DeployButton appID={app.app_id} />
+		  </CardBody>
+	   </Card>
+	   
+	   <Card className="ks-appdetails__sidebar__card">
+		  <CardHeader>
+			 Details
+		  </CardHeader>
+		  
+		  <CardBody>
+			 <div id="ks-appdetails__sidebar__first_section"
+				 className="ks-appdetails__sidebar__section">
+				<div className="ks-appdetails__sidebar__section__title">
+				    Author
+				</div>
+
+				<span>
+				    {app.author.name}
+				    <a href={'mailto:' + app.author.email}>
+					   <span>(<MdEmail />)</span>
+				    </a>
+				</span>
+			 </div>
+			 
+			 <div className="ks-appdetails__sidebar__section">
+				<div className="ks-appdetails__sidebar__section__title">
+				    Links
+				</div>
+
+				<a href="{app.homepage_url}">
+				    <FaLink /> <span>Homepage URL</span>
+				</a>
+				
+				<br />
+				
+				<a href="{app.github_url}">
+				    <FaLink /> <span>Source Code</span>
+				</a>
+			 </div>
+			 <div className="ks-appdetails__sidebar__section">
+				<div className="ks-appdetails__sidebar__section__title">
+				    Categories
+				</div>
+				{getLabels(app.categories)}
+			 </div>
+			 
+			 <div className="ks-appdetails__sidebar__section">
+				<div className="ks-appdetails__sidebar__section__title">
+				    Tags
+				</div>
+				{getLabels(app.tags)}
+			 </div>
+		  </CardBody>
+	   </Card>
+    </div>);
+}
+
+/**
+ * Shows button which triggers modal with serverless app deployment instructions
+ */
+const DeployButton : React.FunctionComponent<DBProps> = (props : DBProps) => {
+    const [isModalOpen,setIsModalOpen] = useState(false);
+    const deployInstructions = useDeployInstructions(props.appID);
+
+    const Code = ((props : any) => (
+	<ClipboardCopy variant={ClipboardCopyVariant.expansion}
+		       isReadOnly>
+	    {props.value}
+	</ClipboardCopy>
+    ));
     
-    const labels :string[] = [
-        "Application","Version","Author","Email"
-    ];
-    const content : string[] = [
-        app_id,version,author.name,author.email
-    ];
-
-
-    return(
-        <Stack className="ks-appdetails__sidebar">
-            <StackItem isFilled={false} className="ks-appdetails__sidebar__stackitem">
-                <DeployButton appID={app_id}></DeployButton>
-            </StackItem>
-            { Map2(labels,content,SidebarStackItem) }
-
-            <StackItem isFilled={false} className="ks-appdetails__sidebar__stackitem">
-                <span className="ks-appdetails__sidebar__stackitem__label">GitHub Repo</span>
-                <a href={github_url} className="ks-appdetails__sidebar__stackitem__content">Click Here</a>
-            </StackItem>
-        </Stack>
+    return (
+	<div>
+         <Button className="ks-appdetails__deploy_button"
+			  onClick={() => setIsModalOpen(true)}variant="primary">
+		   Deploy App
+         </Button>
+	    
+         <Modal isSmall
+			 title="Deployment Instructions"
+			 isOpen={isModalOpen}
+			 onClose={() => { setIsModalOpen(false); }}>
+		
+		<Markdown className="ks-markdown"
+			  renderers={{code : Code}}
+			  source={deployInstructions}/>
+            </Modal>
+	</div>
     );
-
 }
-
-function SidebarStackItem(label : string, content : string){
-    return(
-    <StackItem key={label} isFilled={false} className="ks-appdetails__sidebar__stackitem">
-        <span className="ks-appdetails__sidebar__stackitem__label">{label}</span>
-        <span className="ks-appdetails__sidebar__stackitem__content">{content}</span>
-    </StackItem>)
-}
-
